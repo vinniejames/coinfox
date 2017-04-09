ce87424ce5efe5c0780a0ada4f35ff49bc9abae8
Vinnie James <vincent.depalma@gmail.com>
2017-04-08 22:26:02 -0700

    highchart

--- src/Chart.js
+++ src/Chart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Highcharts from 'highcharts'
 import './Chart.css';
 
 class Chart extends Component {
@@ -6,13 +7,29 @@ class Chart extends Component {
   constructor () {
     super();
     this.state = {
+      chart_container: "chart_container",
       ticker: null,
       chart: {
         Response: "no",
         Data: []
-      }
+      },
+      time_series: [],
+      chart_options: {}
     }
   }
+
+  componentDidMount () {
+        // Set container which the chart should render to.
+        console.log(this.state.chart_container, "put highchart here");
+          this.chart = new Highcharts["Chart"](
+            this.state.chart_container,
+            this.state.chart_options
+          );
+    }
+    //Destroy chart before unmount.
+    componentWillUnmount () {
+        this.chart.destroy();
+    }
 
   _fetchChartData (ticker, currency) {
     const endpoint = 'https://min-api.cryptocompare.com/data/histoday?aggregate=1&e=CCCAGG&extraParams=CryptoCompare&fsym='+ ticker.toUpperCase() +'&limit=365&tryConversion=false&tsym=' + currency.toUpperCase();
@@ -20,9 +37,18 @@ console.log(endpoint);
     fetch(endpoint)
       .then((res) => res.json())
       .then((res)=>{
+        // convert data to time series for highcharts_data
+        const highcharts_data = res.Data.map(function (day) {
+          // highcharts wants timestamp in miliseconds
+          // https://jsfiddle.net/zyqav14L/
+          var fixDate = day.time * 1000;
+          return [fixDate, day.close]
+        })
+
         const nextState = {
           ticker: ticker,
-          chart: res
+          chart: res,
+          time_series: highcharts_data
         }
         this.setState(nextState);
       })
@@ -42,6 +68,8 @@ console.log(endpoint);
     return (
       <div>
         {this.props.ticker} {this.state.chart.Response}
+        <h1>{this.state.time_series.length && this.state.time_series[0][0]}</h1>
+        <div id="chart_container"></div>
       </div>
     )
   }
