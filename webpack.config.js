c10385596f8f6e531aa4899f4465e4b78daac803
Vince DePalma <vincentdepalma@asana.com>
2017-11-20 23:37:02 -0800

    save file for new gaia

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,8 @@ module.exports = {
     watchOptions: { aggregateTimeout: 300, poll: 1000 },
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+      // "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      // "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
     },
   },
   module: {
