8fcd4955fddbf581be18158ddca1c955818c17c5
Vinnie James <vincent.depalma@gmail.com>
2020-07-03 15:13:17 -0400

    Update packages

---
+++ docs/precache-manifest.f237e0115efcbd6c43db192cde186afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c98301716def046d7be52ef3a9a619",
+    "url": "/index.html"
+  },
+  {
+    "revision": "911f9f1c8d812b2cd9f1",
+    "url": "/static/css/2.692d5a42.chunk.css"
+  },
+  {
+    "revision": "03dba3f1a48db9b14acf",
+    "url": "/static/css/main.ce0813bc.chunk.css"
+  },
+  {
+    "revision": "911f9f1c8d812b2cd9f1",
+    "url": "/static/js/2.0535bfdc.chunk.js"
+  },
+  {
+    "revision": "7f57b3cf60e42dd70014ad3c38d6a882",
+    "url": "/static/js/2.0535bfdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03dba3f1a48db9b14acf",
+    "url": "/static/js/main.2acf2d92.chunk.js"
+  },
+  {
+    "revision": "485103438fa728d338e5",
+    "url": "/static/js/runtime-main.ef9ec344.js"
+  }
+]);
