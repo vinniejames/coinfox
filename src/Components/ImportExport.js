ddf3a5e3ef9447dd035f3b7d5ba6b0d48b4dd732
Vincent James <2112033+vinniejames@users.noreply.github.com>
2024-03-11 15:29:49 -0500

    import ios

--- src/Components/ImportExport.js
+++ src/Components/ImportExport.js
@@ -6,18 +6,28 @@ class ImportExport extends Component {
   constructor(){
     super();
     this.state = {
-      importUrl: false
+      importUrl: "Add coins or paste import link"
     };
     this._getLink = this._getLink.bind(this);
+    this._importString = this._importString.bind(this);
   }
 
   _getLink () {
-    if (!localStorage.coinz){
-      alert(translationStrings.addfirstcoin);
-    } else {
+    if (localStorage.coinz){
       const base64 = btoa(JSON.stringify(localStorage));
       this.setState({importUrl: window.location.origin + "?import=" + base64});
     }
+  }
+
+  _importString () {
+    const base64 = this.state.importString;
+    console.log(this.state.importString)
+    const json = atob(base64.split("import=")[1]);
+    const data = JSON.parse(json);
+    Object.keys(data).forEach((key) => {
+      localStorage.setItem(key, data[key]);
+    });
+    window.location.reload();
   }
 
   render() {
@@ -27,10 +37,8 @@ class ImportExport extends Component {
         <hr />
         <h3 className="white center">{string.importexport}</h3>
         <p className="white center">{string.copylink}</p>
-        {!this.state.importUrl
-          ? <p className="center"><button className="btn" onClick={this._getLink}>{string.getlink}</button></p>
-          : <p className="center"><input type="text" defaultValue={this.state.importUrl}></input></p>}
-
+        <p className="center"><input type="text" onChange={(e) => this.setState({importString: e.target.value})} defaultValue={this.state.importUrl}></input></p>
+        {!localStorage.coinz ? <button style={{margin: "8px auto"}} onClick={this._importString}>Import</button> : null}
       </div>
     );
   }
